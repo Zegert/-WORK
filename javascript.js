79d12730f0dc265f9eff8da5371d60ce123e10a4
Zegert Boele <48018787+ZegertBoele@users.noreply.github.com>
2020-02-10 08:39:55 +0100

    Delete javascript.js

--- javascript.js
+++
@@ -1,9 +0,0 @@
-var registreerHier = document.querySelector("#registreerHier");
-
-registreerHier.addEventListener('click', function () {
-    var loginForm = document.querySelector("#registreerHier");
-    var registreerForm = document.querySelector("#LoginHier");
-    console.log(registreerForm);
-    loginForm.classList.toggle('regOn');
-    registreerForm.classList.toggle('regoff');
-});
